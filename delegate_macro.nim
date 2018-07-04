4854de019c5290f3f03fbf0d1c497a9a69a6f9cf
CodeDoes <codedoes@gmail.com>
2018-07-04 04:33:55 +0200

    added parameter

--- delegate_macro.nim
+++ delegate_macro.nim
@@ -1,25 +1,26 @@
 import macros
 import sugar
+import strutils
 
 type
-  Delegate[Proc: proc] = seq[Proc]
+  Delegate[Proc] = seq[Proc]
 
 {.push.}
 {.experimental.}
-macro unpackVarargs*(callee: untyped; args: untyped): untyped =
-  result = newCall(callee)
-  for i in 0 ..< args.len:
-    result.add args[i]
+# macro unpackVarargs*(callee: untyped; args: untyped): untyped =
+#   result = newCall(callee)
+#   for i in 0 ..< args.len:
+#     result.add args[i]
 proc `+=`[T](x: var Delegate[T]; y: T)=
-  x.add y
+  x.add( y)
 proc `-=`[T](x: var Delegate[T]; y: T)=
-  x.del x.find y
-macro add[T](x: var Delegate[T]; y: varargs[T]):untyped=
-  result = newStmtList()
-  for i in y:
-    result.add quote do:
-      system.add(`x`,`i`)
-macro `()`[A](delegate: Delegate[A]; args: varargs):untyped =
+  x.del( x.find y)
+# macro add[T](x: var Delegate[T]; y: varargs[T]):untyped=
+#   result = newStmtList()
+#   for i in y:
+#     result.add quote do:
+#       system.add(`x`,`i`)
+macro `()`[A](delegate: Delegate[A]; args: varargs[typed]):untyped =
   # var ex = newCall(getAst(delegate))
   # result = newStmtList()
   var call_ident = "call".ident
@@ -36,22 +37,15 @@ macro `()`[A](delegate: Delegate[A]; args: varargs):untyped =
 {.pop.}
 # makeDelegate proc(), Event
 
-var onEvent: Delegate[proc()]
+var onEvent: Delegate[proc(k: string)]
 
-onEvent += proc()= echo "1 Hi 1 one"
-onEvent += proc()= echo "2 123122"
+onEvent += proc(k: string)= echo("1 Hi 1 on  " & k)
+onEvent += proc(k: string)= echo("2 12312  " & k)
+onEvent += proc(k: string)= echo("3 zxczxc " & k)
+onEvent += proc(k: string)= echo("4 asdasddzxczxc " & k)
+onEvent += proc(k: string)= echo("5 124523sdfsdzxczxc " & k)
 
-onEvent += 
-  proc()= 
-    echo("3 zxczxc")
-onEvent += 
-  proc()= 
-    echo("4 asdasddzxczxc")
-onEvent += 
-  proc()= 
-    echo("5 124523sdfsdzxczxc")
-
-onEvent()
+onEvent("fasasdasd")
 
 
 
